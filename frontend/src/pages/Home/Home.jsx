33cdfa1bb5fb636b8957ce85b45ef6347298500d
Abir Hasan Al-amin <hasan.alamin.abir+gitHUB@gmail.com>
2024-08-17 21:09:43 +0600

    Add navbar,header,menu-items

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../component/Header/Header'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
+function Home() {
+  const [category,setCategory]=useState('All');
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+    </div>
+  )
+}
+
+export default Home
