3b252f14b08eca6f6d88a34cb57ce51d8e27dad3
Abir Hasan Al-amin <hasan.alamin.abir+gitHUB@gmail.com>
2024-08-20 16:11:15 +0600

    responsive cart

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -23,7 +23,7 @@ function Cart() {
         {food_list.map((item, index) => {
           if (cartItems[item._id] > 0) {
             return (
-              <div>
+              <div key={index}>
                 <div className="cart-items-title cart-items-item">
                   <img src={item.image} />
                   <p>{item.name}</p>
